6585106682998142cdc8623bb6a0966e2f5435ff
sazzad-anwar <sazzadzihan@gmail.com>
2020-07-15 11:09:35 +0600

    new repo

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.route('/')
     const {subscriber_id,password,name} = req.body;
     // if(subscriber_id == cred.subscriber_id && password == cred.password){
         let subscription_amount = 500;
+        let laundry = 30;
+        let food = 20;
+        let others = 10;
+        let total = subscription_amount +laundry+food+others;
         let id = subscriber_id;
         let eblPay = cred['ebl-card-pay'];
         let order_id = Number(randomstring({
@@ -53,11 +57,11 @@ app.route('/')
         let query = db.db.query(sql,[name],(err,found)=>{
             if(err) throw err;
             if(found.length !== 0 && found[0].payment_confirmation == 'SUCCESS'){
-                res.render('dashboard',{order_id,subscription_amount,id, eblPay, due:'0',status:'Paid', name});
+                res.render('dashboard',{order_id,subscription_amount,id, eblPay, due:'0',status:'Paid', name,total,food,laundry,others});
             }else if(found.length !== 0 && found[0].payment_confirmation == 'REQUESTED'){
-                res.render('dashboard',{order_id:found[0].order_id,subscription_amount,id,due:subscription_amount,status:'Due', eblPay, name});
+                res.render('dashboard',{order_id:found[0].order_id,subscription_amount,id,due:subscription_amount,status:'Due', eblPay, name,total,food,laundry,others});
             }else{
-                res.render('dashboard',{order_id,subscription_amount,id,due:subscription_amount,status:'Due', eblPay, name});
+                res.render('dashboard',{order_id,subscription_amount,id,due:subscription_amount,status:'Due', eblPay, name,total,food,laundry,others});
             }
         });
     // }else{
